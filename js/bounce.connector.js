49d5c6ff0f102ce07aacd6007e2e4e9ce4faa830
exratione <reason@exratione.com>
2012-06-09 22:51:04 +0000

    Changing Javascript to use Drupal behaviors.
    Signed-off-by: exratione <reason@exratione.com>

--- js/bounce.connector.js
+++ js/bounce.connector.js
@@ -1,23 +1,25 @@
-jQuery(document).ready(function() {
-  jQuery("#edit-bounce-connector-protocol").change(function(e) {
-    var port = "";
-    switch(jQuery(this).val()) {
-      case "pop3":
-        port = 110;
-        break;
-
-      case "pop3s":
-        port = 995;
-        break;
-
-      case "imap":
-        port = 143;
-        break;
-
-      case "imaps":
-        port = 993;
-        break;
-    }
-    jQuery("#edit-bounce-connector-port").val(port);
-  });
-});
+Drupal.behaviors.bounce = {   
+  attach: function(context, settings) {
+    jQuery("#edit-bounce-connector-protocol", context).change(function(e) {
+      var port = "";
+      switch(jQuery(this).val()) {
+        case "pop3":
+          port = 110;
+          break;
+  
+        case "pop3s":
+          port = 995;
+          break;
+  
+        case "imap":
+          port = 143;
+          break;
+  
+        case "imaps":
+          port = 993;
+          break;
+      }
+      jQuery("#edit-bounce-connector-port", context).val(port);
+    });
+  }
+};
