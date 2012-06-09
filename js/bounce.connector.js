7ab4d1c63dd21b17279e92cdcbc753268362b740
exratione <reason@exratione.com>
2012-06-09 22:53:12 +0000

    Fixing js whitespace.
    Signed-off-by: exratione <reason@exratione.com>

--- js/bounce.connector.js
+++ js/bounce.connector.js
@@ -1,4 +1,4 @@
-Drupal.behaviors.bounce = {   
+Drupal.behaviors.bounce = {
   attach: function(context, settings) {
     jQuery("#edit-bounce-connector-protocol", context).change(function(e) {
       var port = "";
@@ -6,15 +6,15 @@ Drupal.behaviors.bounce = {
         case "pop3":
           port = 110;
           break;
-  
+
         case "pop3s":
           port = 995;
           break;
-  
+
         case "imap":
           port = 143;
           break;
-  
+
         case "imaps":
           port = 993;
           break;
